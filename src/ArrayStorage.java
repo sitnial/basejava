8fd5a4380aa10771dc3c68fcb9436bdf0eaa9129
Alexandra <sitnikovalexandra00@gmail.com>
2021-09-25 21:18:41 +0300

    implement methods create/update/get for ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,36 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    Resume[] storage = {new Resume("first"), new Resume("second"), new Resume("third")};
 
     void clear() {
+        storage = Arrays.stream(storage).filter(resume -> resume != null).toArray(Resume[]::new);
     }
 
     void save(Resume r) {
+        storage = Arrays.copyOf(storage, storage.length + 1);
+        storage[storage.length - 1] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        return Arrays.stream(storage).filter(resume -> uuid.equals(resume.toString())).findAny().orElse(null);
     }
 
     void delete(String uuid) {
+        storage = Arrays.stream(storage).filter(resume -> !uuid.equals(resume.toString())).toArray(Resume[]::new);
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return storage;
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
--- src/Resume.java
+++ src/Resume.java
@@ -6,6 +6,14 @@ public class Resume {
     // Unique identifier
     String uuid;
 
+    public Resume(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public Resume() {
+
+    }
+
     @Override
     public String toString() {
         return uuid;
